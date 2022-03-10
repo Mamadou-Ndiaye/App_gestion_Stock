151060c2e67ccceb5f6770de012e8d31686f979c
Mamadou  NDIAYE <mamadou.ndiaye@gainde2000.sn>
2022-03-10 17:06:42 +0000

    config plus correctionde l Api

--- src/main/java/sn/ucad/gestionstock/controller/api/CommandeFournisseurApi.java
+++ src/main/java/sn/ucad/gestionstock/controller/api/CommandeFournisseurApi.java
@@ -10,16 +10,16 @@ import static sn.ucad.gestionstock.utils.Constatnts.APP_ROOT;
 
 public interface CommandeFournisseurApi {
 
-    @PostMapping(value =APP_ROOT + "/commandeclients/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces =  MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value =APP_ROOT + "/commandefournisseurs/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces =  MediaType.APPLICATION_JSON_VALUE)
     CommandeFournisseurDto save(@RequestBody  CommandeFournisseurDto commandeFournisseurDto);
 
-    @GetMapping(value =  APP_ROOT+ "/commandeclients/{idCmdFournisseur}" , produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value =  APP_ROOT+ "/commandefournisseurs/{idCmdFournisseur}" , produces = MediaType.APPLICATION_JSON_VALUE)
     CommandeFournisseurDto  findById(@PathVariable("idCmdFournisseur") Long id);
 
-    @GetMapping(value =  APP_ROOT+ "/commandeclients/all" , produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value =  APP_ROOT+ "/commandefournisseurs/all" , produces = MediaType.APPLICATION_JSON_VALUE)
     List<CommandeFournisseurDto> findAll();
 
-    @GetMapping(value = APP_ROOT + "/commandeclients/{codeCmdFournisseur}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = APP_ROOT + "/commandefournisseurs/{codeCmdFournisseur}", produces = MediaType.APPLICATION_JSON_VALUE)
     CommandeFournisseurDto findByCode(@PathVariable("codeCmdFournisseur") String code);
 
     @DeleteMapping(value = APP_ROOT + "/commandefournisseurs/delete/{idCmdFournisseur}")
